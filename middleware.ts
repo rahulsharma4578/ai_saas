ee96343d6b5048076aada8d3002b8aa8803a1609
rahuls <sharmarahul020899@gmail.com>
2024-05-15 14:59:09 +0530

    First Commit

---
+++ middleware.ts
@@ -0,0 +1,11 @@
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+export default clerkMiddleware();
+
+export const config = {
+  matcher: [
+    '/((?!.*\\..*|_next).*)', // Exclude static files and _next folder
+    '/', // Allow the homepage
+    '/api/webhooks/clerk', // Allow the /api/webhooks/clerk route
+  ],
+};

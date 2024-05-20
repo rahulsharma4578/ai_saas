03939f2312b14c5ed300d1272115a05a70ffa10c
rahuls <sharmarahul020899@gmail.com>
2024-05-20 12:47:59 +0530

    Cloudinary Media Uploader included

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 
 import MobileNav from '@/components/shared/MobileNav'
 import Sidebar from '@/components/shared/Sidebar'
+import { Toaster } from '@/components/ui/toaster'
 import React from 'react'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
@@ -15,6 +16,8 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
           </div>
 
       </div>
+
+      <Toaster />
         
     </main>
   )
--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -28,6 +28,7 @@ import { CustomField } from "./CustomField"
 import { useState, useTransition } from "react"
 import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils"
 import { config } from "process"
+import MediaUploader from "./MediaUploader"
 
 export const formSchema = z.object({
   title: z.string(),
@@ -149,6 +150,7 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance, 
             )}  
           />
         )}
+
         {(type === 'remove' || type === 'recolor') && (
           <div className="prompt-field">
             <CustomField 
@@ -171,6 +173,7 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance, 
                 />
               )}
             />
+
             {type === 'recolor' && (
               <CustomField 
                 control={form.control}
@@ -193,8 +196,26 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance, 
             )}
           </div>
         )}
+        
+        <div className="media-uploader-field">
+          <CustomField 
+            control={form.control}
+            name="publicId"
+            className="flex size-full flex-col"
+            render={({ field }) => (
+              <MediaUploader 
+                onValueChange={field.onChange}
+                setImage={setImage}
+                publicId={field.value}
+                image={image}
+                type={type}
+              />
+            )}
+          />
+        </div>
+
       
-      <div className="flex flex-col gap-4">
+       <div className="flex flex-col gap-4">
           <Button 
             type="button"
             className="submit-button capitalize"
